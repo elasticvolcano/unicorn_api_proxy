d67bca8d0c5b6a4c7553dcf3db7ed888adaabb21
Unicorn Rentals <elasticvolcano@gmail.com>
2021-02-25 13:47:40 +0000

    Apparently works-for-me is a crappy excuse

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'a667547553432a008f92ec0104a7d421'
-__version__ = 'cbfc72e466a87ca18e3f025e78dcbd92'
+__vcs_id__ = 'ffcba94deea4f474240f77921d0fff6a'
+__version__ = 'ae364ff4bfeba8651cd6e5ead611168d'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
